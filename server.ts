c242afa30c0384c0a64121741707a8b6eced422f
anasryadi <anasryadi0@gmail.com>
2022-12-05 19:07:26 -0800

    installed dotenv and fetched the data from the backend to App.tsx

--- server.ts
+++ server.ts
@@ -1,19 +1,31 @@
 import express, { Request, Response } from 'express'
 import axios, { AxiosResponse } from 'axios'
+import { QuizData } from './interfaces'
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 const PORT = 8000
 const app = express()
 
 app.get("/quiz/item", async(req: Request, res: Response) => {
     try {
-        const respons : AxiosResponse = await axios.get("https://08b96e2e-44eb-4335-9944-abf0d8a36672-us-east1.apps.astra.datastax.com/api/rest/v2/namespaces/quizes/collections/quirky",
+        // @ts-ignore
+        const response: AxiosResponse = await axios.get(process.env.URL,
         {
             headers: {
-                "X-Cassandra-Token: AstraCS:MbeGukAIEqZIoHykJbrvnidL:273d2e106ec957ebe22cd8b75b3d5f8efcace2f29f632387f40c05b234b57cfd",
+                "X-Cassandra-Token": process.env.TOKEN,
                 "accept" : "application/json"
             }
         }
         )
+        if (response.status === 200) {
+            const quizItem: QuizData = await response.data.data['2abb02e9-8aea-4ef6-a662-a13293f202d6']
+            res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+            res.send(quizItem)
+        }
+    }
+    catch (e) {
+        console.log("Something went wrong and here is why: ", e)
     }
 })
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Title from './components/Title';
+import { QuizData } from '../interfaces'
 
 const App = () => {
+  const [quiz, setQuiz] = useState<QuizData | null>()
+
+  const fetchData = async () => {
+    try {
+      const reponse = await fetch('http://localhost:8000/quiz/item')
+      const json = await reponse.json()
+      setQuiz(json)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  console.log(quiz)
+
   return (
     <div>
       <Title />
